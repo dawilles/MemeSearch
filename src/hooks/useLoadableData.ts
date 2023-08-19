fbefe263f2830d035f293c8a19bf0bdf1baa4ceb
Dawid <104320230+dawilles@users.noreply.github.com>
2023-08-19 13:02:45 +0200

    working: 80%

--- src/hooks/useLoadableData.ts
+++ src/hooks/useLoadableData.ts
@@ -4,7 +4,6 @@ import { notReachable } from "../utils/notReachable";
 
 export type LoadableData<Data, Params, E = AxiosError<Error>> =
 	| { type: "loading"; params: Params }
-	// | { type: "appending"; previousData: Data; params: Params }
 	| { type: "loaded"; data: Data; params: Params }
 	| { type: "error"; error: E; params: Params };
 
@@ -32,7 +31,6 @@ export const useLoadableData = <
 			switch (state.type) {
 				case "loading":
 				case "loaded":
-				// case "appending":
 				case "error":
 					setState({ type: "loading", params: paramsToUse });
 					break;
@@ -56,7 +54,6 @@ export const useLoadableData = <
 				break;
 			}
 			case "loaded":
-			// case "appending":
 			case "error":
 				break;
 			default:
@@ -69,206 +66,3 @@ export const useLoadableData = <
 		reload,
 	};
 };
-//============good code================
-// import { useState, useCallback, useEffect } from "react";
-// import { AxiosError, AxiosRequestConfig } from "axios";
-// import { notReachable } from "../utils/notReachable";
-
-// export type LoadableData<Data, Params, E = AxiosError<Error>> =
-// 	| { type: "loading"; params: Params }
-// 	| { type: "loaded"; data: Data; params: Params }
-// 	| { type: "error"; error: E; params: Params };
-
-// type ReturnType<Data, Params, Error> = {
-// 	state: LoadableData<Data, Params, Error>;
-// 	reload: (newParams?: Params) => void;
-// };
-
-// export const useLoadableData = <
-// 	Data,
-// 	Params = undefined,
-// 	E = AxiosError<Error>,
-// >(
-// 	load: (params: Params, config?: AxiosRequestConfig) => Promise<Data>,
-// 	initialParams: Params,
-// ): ReturnType<Data, Params, E> => {
-// 	const [state, setState] = useState<LoadableData<Data, Params, E>>({
-// 		type: "loading",
-// 		params: initialParams,
-// 	});
-
-// 	const reload = useCallback((newParams?: Params) => {
-// 		if (newParams) {
-// 			setState({ type: "loading", params: newParams });
-// 		} else {
-// 			setState((prevState) => ({ ...prevState, type: "loading" }));
-// 		}
-// 	}, []);
-
-// 	useEffect(() => {
-// 		switch (state.type) {
-// 			case "loading": {
-// 				load(state.params)
-// 					.then((data) => {
-// 						setState({ type: "loaded", data: data, params: state.params });
-// 					})
-// 					.catch((error) => {
-// 						setState({ type: "error", error, params: state.params });
-// 					});
-// 				break;
-// 			}
-// 			case "loaded":
-// 			case "error":
-// 				break;
-// 			default:
-// 				return notReachable(state);
-// 		}
-// 	}, [load, state]);
-
-// 	return {
-// 		state,
-// 		reload,
-// 	};
-// };
-//========Alex code================
-// import { useState, useCallback, useEffect } from "react";
-// import { AxiosError, AxiosRequestConfig } from "axios";
-// import { notReachable } from "../utils/notReachable";
-
-// export type LoadableData<Data, Params, E = AxiosError<Error>> =
-// 	| { type: "loading"; params: Params }
-// 	| { type: "loaded"; data: Data; params: Params }
-// 	| { type: "error"; error: E; params: Params };
-
-// type ReturnType<Data, Params, Error> = {
-// 	state: LoadableData<Data, Params, Error>;
-// 	reload: () => void;
-// };
-
-// export const useLoadableData = <
-// 	Data,
-// 	Params = undefined,
-// 	E = AxiosError<Error>,
-// >(
-// 	load: (params: Params, config?: AxiosRequestConfig) => Promise<Data>,
-// 	params: Params,
-// ): ReturnType<Data, Params, E> => {
-// 	const [state, setState] = useState<LoadableData<Data, Params, E>>({
-// 		type: "loading",
-// 		params: params,
-// 	});
-
-// 	const reload = useCallback(() => {
-// 		switch (state.type) {
-// 			case "loading":
-// 			case "loaded":
-// 			case "error":
-// 				setState({ type: "loading", params });
-// 				break;
-// 			default:
-// 				return notReachable(state);
-// 		}
-// 	}, [state, params]);
-
-// 	useEffect(() => {
-// 		if (
-// 			params !== state.params &&
-// 			JSON.stringify(params) !== JSON.stringify(state.params)
-// 		) {
-// 			reload();
-// 		}
-// 	}, [state, params, reload]);
-
-// 	useEffect(() => {
-// 		switch (state.type) {
-// 			case "loading":
-// 				load(state.params)
-// 					.then((data) => {
-// 						setState({ type: "loaded", data: data, params: state.params });
-// 					})
-// 					.catch((error) => {
-// 						setState({ type: "error", error, params: state.params });
-// 					});
-// 				break;
-// 			case "loaded":
-// 			case "error":
-// 				break;
-// 			default:
-// 				return notReachable(state);
-// 		}
-// 	}, [load, state]);
-
-// 	return {
-// 		state,
-// 		reload,
-// 	};
-// };
-//----- try
-// import { useState, useCallback, useEffect } from "react";
-// import { AxiosError, AxiosRequestConfig } from "axios";
-// import { notReachable } from "../utils/notReachable";
-
-// export type LoadableData<Data, Params, E = AxiosError<Error>> =
-// 	| { type: "loading"; params: Params }
-// 	| { type: "loaded"; data: Data; params: Params }
-// 	| { type: "error"; error: E; params: Params };
-
-// type ReturnType<Data, Params, Error> = {
-// 	state: LoadableData<Data, Params, Error>;
-// 	reload: (newParams?: Params) => void;
-// };
-
-// export const useLoadableData = <
-// 	Data,
-// 	Params = undefined,
-// 	E = AxiosError<Error>,
-// >(
-// 	load: (params: Params, config?: AxiosRequestConfig) => Promise<Data>,
-// 	initialParams: Params,
-// ): ReturnType<Data, Params, E> => {
-// 	const [state, setState] = useState<LoadableData<Data, Params, E>>({
-// 		type: "loading",
-// 		params: initialParams,
-// 	});
-
-// 	const reload = useCallback(
-// 		(newParams?: Params) => {
-// 			const paramsToUse = newParams || state.params;
-// 			setState({ type: "loading", params: paramsToUse });
-// 		},
-// 		[state],
-// 	);
-
-// 	useEffect(() => {
-// 		if (
-// 			initialParams !== state.params &&
-// 			JSON.stringify(initialParams) !== JSON.stringify(state.params)
-// 		) {
-// 			reload();
-// 		}
-// 	}, [state, initialParams, reload]);
-
-// 	useEffect(() => {
-// 		switch (state.type) {
-// 			case "loading":
-// 				load(state.params)
-// 					.then((data) => {
-// 						setState({ type: "loaded", data: data, params: state.params });
-// 					})
-// 					.catch((error) => {
-// 						setState({ type: "error", error, params: state.params });
-// 					});
-// 				break;
-// 			case "loaded":
-// 			case "error":
-// 				break;
-// 			default:
-// 				return notReachable(state);
-// 		}
-// 	}, [load, state]);
-
-// 	return {
-// 		state,
-// 		reload,
-// 	};
-// };
